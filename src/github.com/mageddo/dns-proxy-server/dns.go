c34ef12f87276c96a4e37188f33b4bb27b32d268
Elvis de Freitas <edigitalb@gmail.com>
2017-01-31 01:48:33 -0200

    MG-207 setup docker containers cache

--- src/github.com/mageddo/dns-proxy-server/dns.go
+++ src/github.com/mageddo/dns-proxy-server/dns.go
@@ -13,6 +13,7 @@ import (
 	"github.com/mageddo/dns-proxy-server/proxy"
 	"reflect"
 	"github.com/mageddo/dns-proxy-server/utils"
+	"github.com/mageddo/dns-proxy-server/events"
 )
 
 var (
@@ -73,15 +74,18 @@ func handleQuestion(respWriter dns.ResponseWriter, reqMsg *dns.Msg) {
 
 }
 
+const serverPort = 8980
+
 func serve(net, name, secret string) {
+	var port string = fmt.Sprintf(":%d", serverPort)
 	switch name {
 	case "":
-		server := &dns.Server{Addr: ":53", Net: net, TsigSecret: nil}
+		server := &dns.Server{Addr: port, Net: net, TsigSecret: nil}
 		if err := server.ListenAndServe(); err != nil {
 			fmt.Printf("Failed to setup the "+net+" server: %s\n", err.Error())
 		}
 	default:
-		server := &dns.Server{Addr: ":53", Net: net, TsigSecret: map[string]string{name: secret}}
+		server := &dns.Server{Addr: port, Net: net, TsigSecret: map[string]string{name: secret}}
 		if err := server.ListenAndServe(); err != nil {
 			fmt.Printf("Failed to setup the "+net+" server: %s\n", err.Error())
 		}
@@ -108,6 +112,7 @@ func main() {
 	}
 
 	dns.HandleFunc(".", handleQuestion)
+	go events.HandleDockerEvents()
 	go serve("tcp", name, secret)
 	go serve("udp", name, secret)
 	sig := make(chan os.Signal)
