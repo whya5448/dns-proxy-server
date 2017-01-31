230253f57cadd3f80482e1d09ea47f43a36465fe
Elvis de Freitas <edigitalb@gmail.com>
2017-01-31 02:29:19 -0200

    MG-207 fixing some compilation erros

--- src/github.com/mageddo/dns-proxy-server/proxy/DockerDnsSolver.go
+++ src/github.com/mageddo/dns-proxy-server/proxy/DockerDnsSolver.go
@@ -17,25 +17,26 @@ type DockerDnsSolver struct {
 func (DockerDnsSolver) Solve(question dns.Question) (*dns.Msg, error) {
 
 	log.Logger.Infof("m=solve, status=begin, solver=docker, name=%s", question.Name)
+	key := question.Name[:len(question.Name)-1]
+	if events.ContainsKey(key) {
 
-	if events.ContainsKey(question.Name) {
-
-		ip := events.Get(question.Name)
+		ip := events.Get(key)
 		ipArr := strings.Split(ip, ".")
 		i1, _ := strconv.Atoi(ipArr[0])
 		i2, _ := strconv.Atoi(ipArr[1])
 		i3, _ := strconv.Atoi(ipArr[2])
 		i4, _ := strconv.Atoi(ipArr[3])
 
-		m := new(dns.Msg)
-		m.Answer = dns.A{
+		rr := &dns.A{
 			Hdr: dns.RR_Header{Name: question.Name, Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: 0},
-			A: net.IPv4(i1, i2, i3, i4),
+			A: net.IPv4(byte(i1), byte(i2), byte(i3), byte(i4)),
 		}
+
+		m := new(dns.Msg)
+		m.Answer = append(m.Answer, rr)
+		log.Logger.Infof("m=solve, status=success, solver=docker")
 		return m, nil
 	}
+	log.Logger.Errorf("m=solve, status=error, solver=docker")
 	return nil, errors.New("hostname not found")
-
-	log.Logger.Infof("m=solve, status=success, solver=docker")
-
 }
