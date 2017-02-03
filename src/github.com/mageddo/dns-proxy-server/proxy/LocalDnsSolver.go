4a5d139873ca89700d00a89707dbdbe0487e8e0b
Elvis de Freitas <edigitalb@gmail.com>
2017-02-03 00:36:35 -0200

    MG-212 implementing local resolver

--- src/github.com/mageddo/dns-proxy-server/proxy/LocalDnsSolver.go
+++ src/github.com/mageddo/dns-proxy-server/proxy/LocalDnsSolver.go
@@ -3,6 +3,9 @@ package proxy
 import (
 	"errors"
 	"github.com/miekg/dns"
+	"net"
+	"github.com/mageddo/dns-proxy-server/events/local"
+	"github.com/mageddo/log"
 )
 
 type LocalDnsSolver struct {
@@ -10,6 +13,20 @@ type LocalDnsSolver struct {
 }
 
 func (LocalDnsSolver) Solve(question dns.Question) (*dns.Msg, error) {
-	// procura no json local ou base sqlite
-	return nil, errors.New("not implemented")
+
+	key := question.Name[:len(question.Name)-1]
+	activeEnv := events.GetConfiguration().GetActiveEnv()
+	hostname := activeEnv.GetHostname(key)
+	if  hostname != nil {
+		rr := &dns.A{
+			Hdr: dns.RR_Header{Name: question.Name, Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: 0},
+			A: net.IPv4(hostname.Ip[0], hostname.Ip[1], hostname.Ip[2], hostname.Ip[3]),
+		}
+
+		m := new(dns.Msg)
+		m.Answer = append(m.Answer, rr)
+		log.Logger.Infof("m=solve, status=success, solver=local")
+		return m, nil
+	}
+	return nil, errors.New("hostname not found")
 }
--- src/github.com/mageddo/dns-proxy-server/utils/dns.go
+++ src/github.com/mageddo/dns-proxy-server/utils/dns.go
@@ -3,6 +3,7 @@ package utils
 type LocalConfiguration struct {
 	RemoteDnsServers [][4]byte
 	Envs []EnvVo
+	ActiveEnv string
 }
 
 type EnvVo struct {
@@ -11,6 +12,25 @@ type EnvVo struct {
 }
 
 type HostnameVo struct {
-	ip [4]byte
-	ttl int
+	Hostname string
+	Ip [4]byte
+	Ttl int
+}
+func (lc *LocalConfiguration) GetActiveEnv() EnvVo {
+
+	for _, env := range lc.Envs {
+		if env.Name == lc.ActiveEnv {
+			return env
+		}
+	}
+	return nil
+}
+
+func(env *EnvVo) GetHostname(hostname string) HostnameVo {
+	for _, host := range env.Hostnames {
+		if host.Hostname == hostname {
+			return host
+		}
+	}
+	return nil
 }
