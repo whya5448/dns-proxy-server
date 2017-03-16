8a6dba72704a3df87f5484348b52a004c9af2b26
Elvis de Freitas <edigitalb@gmail.com>
2017-03-15 22:02:59 -0300

    MG-206 refatoring hostname apis package

--- src/github.com/mageddo/dns-proxy-server/controller/index.go
+++ src/github.com/mageddo/dns-proxy-server/controller/index.go
@@ -2,49 +2,11 @@ package controller
 
 import (
 	"net/http"
-	"github.com/mageddo/dns-proxy-server/events/local"
-	"encoding/json"
 	"golang.org/x/net/context"
-	"github.com/mageddo/log"
 )
 
 func init(){
 	Get("/hello/", func(ctx context.Context, res http.ResponseWriter, req *http.Request, url string){
 		res.Write([]byte("It works from controller!!!"))
-	})
-
-	Get("/hostname/", func(ctx context.Context, res http.ResponseWriter, req *http.Request, url string){
-		res.Header().Add("Content-Type", "application/json")
-		json.NewEncoder(res).Encode(local.GetConfiguration(ctx))
-	})
-
-	Post("/hostname/", func(ctx context.Context, res http.ResponseWriter, req *http.Request, url string){
-		logger := log.GetLogger(ctx)
-		res.Header().Add("Content-Type", "application/json")
-		logger.Infof("m=/hostname/, status=begin, action=create-hostname")
-		var hostname local.HostnameVo
-		json.NewDecoder(req.Body).Decode(&hostname)
-		logger.Infof("m=/hostname/, status=parsed-host, host=%+v", hostname)
-		err := local.GetConfiguration(ctx).AddHostname(ctx, hostname.Env, hostname)
-		if err != nil {
-			logger.Infof("m=/hostname/, status=error, action=create-hostname, err=%+v", err)
-			BadRequest(res, err.Error())
-		}
-		logger.Infof("m=/hostname/, status=success, action=create-hostname")
-	})
-
-	Delete("/hostname/", func(ctx context.Context, res http.ResponseWriter, req *http.Request, url string){
-		logger := log.GetLogger(ctx)
-		res.Header().Add("Content-Type", "application/json")
-		logger.Infof("m=/hostname/, status=begin, action=delete-hostname")
-		var hostname local.HostnameVo
-		json.NewDecoder(req.Body).Decode(&hostname)
-		logger.Infof("m=/hostname/, status=parsed-host, action=delete-hostname, host=%+v", hostname)
-		err := local.GetConfiguration(ctx).RemoveHostnameByEnvAndHostname(ctx, hostname.Env, hostname.Hostname)
-		if err != nil {
-			logger.Infof("m=/hostname/, status=error, action=delete-hostname, err=%+v", err)
-			BadRequest(res, err.Error())
-		}
-		logger.Infof("m=/hostname/, status=success, action=delete-hostname")
 	})
 }
