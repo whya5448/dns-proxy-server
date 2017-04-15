f7da455b894a516b37c931db21f8f37872818e2f
Elvis de Freitas <edigitalb@gmail.com>
2017-04-15 15:28:05 -0300

    MG-214 adding test framework

--- src/github.com/mageddo/dns-proxy-server/conf/conf_test.go
+++ src/github.com/mageddo/dns-proxy-server/conf/conf_test.go
@@ -18,20 +18,58 @@ func TestGetCurrentIpAddress(t *testing.T){
 
 }
 
-func TestSetMachineDNSServer(t *testing.T) {
+func TestSetMachineDNSServer_EmptyFileSuccess(t *testing.T) {
 
-	//x := os.Open("/tmp/test-resolv.conf")
+	const TMP_RESOLV_FILE = "/tmp/test-resolv.conf"
 
-
-	os.Setenv(env.MG_RESOLVCONF, "/tmp/test-resolv.conf")
+	os.OpenFile(TMP_RESOLV_FILE, os.O_TRUNC, 0666)
+	os.Setenv(env.MG_RESOLVCONF, TMP_RESOLV_FILE)
 	err := SetMachineDNSServer("9.9.9.9")
 	if err != nil {
 		t.Error(err)
 	}
-	bytes, err := ioutil.ReadFile("/tmp/test-resolv.conf")
+	bytes, err := ioutil.ReadFile(TMP_RESOLV_FILE)
 	if err != nil {
 		t.Error(err)
 	}
 	fmt.Println(string(bytes))
 
+	// Output: nameserver 9.9.9.9 # dns-proxy-server
+
+}
+
+func TestSetMachineDNSServer_WithPreviousDnsServerAndCommentSuccess(t *testing.T) {
+
+	const TMP_RESOLV_FILE = "/tmp/test-resolv.conf"
+	os.Setenv(env.MG_RESOLVCONF, TMP_RESOLV_FILE)
+
+	err := ioutil.WriteFile(TMP_RESOLV_FILE, []byte("# Provided by test\nnameserver 8.8.8.8"), 0666)
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = SetMachineDNSServer("9.9.9.9")
+	if err != nil {
+		t.Error(err)
+	}
+	bytes, err := ioutil.ReadFile(TMP_RESOLV_FILE)
+	if err != nil {
+		t.Error(err)
+	}
+	fmt.Println(string(bytes))
+
+	AssertEqual(t, `nameserver 9.9.9.9 # dns-proxy-server
+	# Provided by test
+	nameserver 8.8.8.8`, string(bytes), "not match")
+
+}
+
+func AssertEqual(t *testing.T, a interface{}, b interface{}, message string) {
+	if a == b {
+		return
+	}
+	if len(message) == 0 {
+		message = fmt.Sprintf("%v != %v", a, b)
+	}
+	t.Fatal(message)
 }
