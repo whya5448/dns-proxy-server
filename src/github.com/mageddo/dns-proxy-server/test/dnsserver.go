89766dd39795813a56d8122135036f9a90f26404
Elvis de Freitas <edigitalb@gmail.com>
2017-07-12 23:38:11 -0300

    MG-337 fixing test

--- src/github.com/mageddo/dns-proxy-server/test/dnsserver.go
+++ src/github.com/mageddo/dns-proxy-server/test/dnsserver.go
@@ -41,7 +41,8 @@ import (
 	"syscall"
 	"github.com/miekg/dns"
 	"github.com/mageddo/log"
-	"github.com/mageddo/dns-proxy-server/utils"
+	"net"
+	"errors"
 )
 
 var (
@@ -68,7 +69,7 @@ func handleReflect(respWriter dns.ResponseWriter, reqMsg *dns.Msg) {
 
 	log.Logger.Infof("m=handleReflect, questions=%d, 1stQuestion=%s", len(reqMsg.Question), questionName)
 
-	resp := utils.SolveName(questionName)
+	resp := SolveName(questionName)
 	resp.SetReply(reqMsg)
 	resp.Compress = *compress
 
@@ -124,4 +125,34 @@ func main2() {
 	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
 	s := <-sig
 	fmt.Printf("Signal (%s) received, stopping\n", s)
+}
+
+
+// reference https://miek.nl/2014/August/16/go-dns-package/
+func SolveName(hostname string) *dns.Msg {
+
+
+	//config, _ := dns.ClientConfigFromFile("/etc/resolv.conf")		
+	c := new(dns.Client)
+
+	m := new(dns.Msg)
+	m.SetQuestion(dns.Fqdn(hostname), dns.TypeA) // CAN BE A, AAA, MX, etc.		
+	m.RecursionDesired = true
+
+	//r, _, err := c.Exchange(m, net.JoinHostPort(config.Servers[0], config.Port)) // server and port to ask		
+	r, _, err := c.Exchange(m, net.JoinHostPort("8.8.8.8", "53")) // server and port to ask		
+
+	// if the answer not be returned		
+	if r == nil {
+		panic(err)
+	}
+
+	// what the code of the return message ?		
+	if r.Rcode != dns.RcodeSuccess {
+		panic(errors.New(fmt.Sprintf(" *** invalid answer name %s after MX query for %s", hostname, hostname)))
+	}
+
+	// looping through the anwsers		
+	return r
+
 }
