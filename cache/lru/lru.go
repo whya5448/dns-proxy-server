d1cfeac6629f4349ad262607810ad6e4e8ce77ba
Elvis de Freitas Souza <edigitalb@gmail.com>
2017-10-02 15:46:49 -0300

    #42 implementing size limitation

--- cache/lru/lru.go
+++ cache/lru/lru.go
@@ -1,12 +1,17 @@
 package lru
 
-import "github.com/mageddo/dns-proxy-server/cache"
+import (
+	. "github.com/mageddo/dns-proxy-server/log"
+	"github.com/mageddo/dns-proxy-server/cache"
+	"container/list"
+)
 
 type LRUCache struct {
 	name string
+	size int
 	timeout int64
-	size int64
 	cache map[interface{}]interface{}
+	keyset *list.List
 }
 
 func (c *LRUCache) GetName() string {
@@ -18,6 +23,15 @@ func (c *LRUCache) Get(key interface{}) interface{} {
 }
 
 func (c *LRUCache) Put(key, value interface{}) {
+	if c.size > 0 {
+		if c.keyset.Len() == c.size {
+			lastKey := c.keyset.Back()
+			LOGGER.Debugf("status=size-limit-reached, size=%d, key=%v, keyToRemove=%v", c.size, key, lastKey.Value)
+			c.keyset.Remove(lastKey)
+			delete(c.cache, lastKey.Value)
+		}
+		c.keyset.PushFront(key)
+	}
 	c.cache[key] = value
 }
 
@@ -26,7 +40,7 @@ func (c *LRUCache) Put(key, value interface{}) {
 // size is the maximum size of the cache, -1 if it is unlimited
 // time time in millis before cache expires, -1 if it is unlimited
 //
-func NewLRUCache(name string, size, timeout int64) cache.Cache {
-	return &LRUCache{name, timeout, size, make(map[interface{}]interface{})}
+func NewLRUCache(name string, size int, timeout int64) cache.Cache {
+	return &LRUCache{name, size, timeout, make(map[interface{}]interface{}), list.New()}
 }
 
--- cache/lru/lru_test.go
+++ cache/lru/lru_test.go
@@ -13,3 +13,16 @@ func TestPutAndGetSuccess(t *testing.T){
 	assert.Equal(t, "value1", cache.Get("key1").(string))
 
 }
+
+func TestPutAndGetSuccessSizeLimited(t *testing.T){
+
+	cache := NewLRUCache("test1", 2, -1);
+	cache.Put("key1", "value1");
+	cache.Put("key2", "value2");
+	cache.Put("key3", "value3");
+
+	assert.Nil(t, cache.Get("key1"))
+	assert.Equal(t, "value2", cache.Get("key2").(string))
+	assert.Equal(t, "value3", cache.Get("key3").(string))
+
+}
