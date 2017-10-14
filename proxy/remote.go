d1aba1088e8e4378faf5e5c0fbc4988440595557
Elvis de Freitas <edigitalb@gmail.com>
2017-10-14 02:06:07 -0300

    #42 testing remote resolution

--- proxy/remote.go
+++ proxy/remote.go
@@ -14,10 +14,12 @@ import (
 
 const SERVERS = "SERVERS"
 
-type RemoteDnsSolver struct {}
+type RemoteDnsSolver struct {
+	confloader func(ctx context.Context) (*local.LocalConfiguration, error)
+}
 
 // reference https://miek.nl/2014/August/16/go-dns-package/
-func (RemoteDnsSolver) Solve(ctx context.Context, question dns.Question) (*dns.Msg, error) {
+func (r RemoteDnsSolver) Solve(ctx context.Context, question dns.Question) (*dns.Msg, error) {
 	c := store.GetInstance()
 	logger := log.NewLog(ctx)
 	client := new(dns.Client)
@@ -29,7 +31,7 @@ func (RemoteDnsSolver) Solve(ctx context.Context, question dns.Question) (*dns.M
 	var err error
 	if !c.ContainsKey(SERVERS) {
 		logger.Debugf("status=hot-load")
-		if config, err = local.LoadConfiguration(ctx); err != nil {
+		if config, err = r.confloader(ctx); err != nil {
 			logger.Errorf("error=%v",err)
 			return nil, err
 		}
@@ -53,17 +55,24 @@ func (RemoteDnsSolver) Solve(ctx context.Context, question dns.Question) (*dns.M
 		var r *dns.Msg
 		r, _, err = client.Exchange(m, net.JoinHostPort(formatServer, "53"))
 
-			// if the answer not be returned
-			if r == nil {
-				err = errors.New(fmt.Sprintf("status=answer-can-not-be-null, err=%v", err))
-				logger.Infof("status=no-answer, err=%s", err)
-				continue
-			} else if r.Rcode != dns.RcodeSuccess { // what the code of the return message ?
-				err = errors.New(fmt.Sprintf("status=invalid-answer-name, name=%s, rcode=%d", question.Name, r.Rcode))
-				logger.Infof("status=bad-code, name=%s, rcode=%d, err=%s", question.Name, r.Rcode, err)
-				continue
-			}
-			return r, nil
+		// if the answer not be returned
+		if r == nil {
+			err = errors.New(fmt.Sprintf("status=answer-can-not-be-null, err=%v", err))
+			logger.Infof("status=no-answer, err=%s", err)
+			continue
+		} else if r.Rcode != dns.RcodeSuccess { // what the code of the return message ?
+			err = errors.New(fmt.Sprintf("status=invalid-answer-name, name=%s, rcode=%d", question.Name, r.Rcode))
+			logger.Infof("status=bad-code, name=%s, rcode=%d, err=%s", question.Name, r.Rcode, err)
+			continue
 		}
-		return nil, err
+		return r, nil
 	}
+	return nil, err
+}
+
+func NewRemoteDnsSolver() *RemoteDnsSolver {
+	return &RemoteDnsSolver{
+		confloader: func(ctx context.Context) (*local.LocalConfiguration, error) {
+		return local.LoadConfiguration(ctx)
+	}}
+}
