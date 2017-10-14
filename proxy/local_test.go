6728c1b79ce6f96b63b8130b84b086416f742c96
Elvis de Freitas <edigitalb@gmail.com>
2017-10-14 13:02:47 -0300

    #42 creating test for local solver

--- proxy/local_test.go
+++ proxy/local_test.go
@@ -13,18 +13,6 @@ import (
 	"github.com/mageddo/dns-proxy-server/cache/store"
 )
 
-type MockCache struct {
-	mock.Mock
-	lru.LRUCache
-}
-
-func (m *MockCache) Get(key interface{}) interface{} {
-	fmt.Println("mocked!!!!!")
-	m.Called(key)
-	return m.LRUCache.Get(key)
-	//return args.Get(0)
-}
-
 func TestLocalDnsSolver_Solve(t *testing.T) {
 
 	ctx := logging.NewContext()
@@ -49,28 +37,51 @@ func TestLocalDnsSolver_Solve(t *testing.T) {
 
 }
 
+
+type MockCache struct {
+	mock.Mock
+	lru.LRUCache
+}
+
+//
+// spy put method
+//
+func (m *MockCache) PutIfAbsent(key, value interface{}) interface{} {
+	fmt.Println("mocked!!!!!")
+	m.Called(key, value)
+	return m.LRUCache.PutIfAbsent(key, value)
+}
+
+//
+// Testing if cache is working
+// In first time must load hostname from file
+// In second must load from cache
+//
 func TestLocalDnsSolver_SolveValidatingCache(t *testing.T) {
 
 	ctx := logging.NewContext()
 	conf, err := local.LoadConfiguration(ctx)
 	assert.Nil(t, err, "failed to load configuration")
 
+	// configuring a new host at local configuration
 	expectedHostname := "github.com"
 	host := local.HostnameVo{Hostname: expectedHostname, Env:"", Ttl:50, Ip:[4]byte{192,168,0,1}}
 	conf.AddHostname(ctx, "", host)
 
+	// creating a request for the created host
 	question := new(dns.Question)
 	question.Name = expectedHostname + "."
 
+	// stubbing cache to verify the calls
 	mockCache := &MockCache{}
 	mockCache.Cache, err = hashlru.New(1)
 	assert.Nil(t, err, "Failed to create cache")
+	mockCache.On("PutIfAbsent", expectedHostname, mock.Anything).Once()
+
 	solver := NewLocalDNSSolver(mockCache)
 
-	mockCache.On("Get", expectedHostname).Twice()
-
-
-	for i:=2; i > 0; i-- {
+	// we ask for the same host 5 times but it must load from file just once
+	for i:=5; i > 0; i-- {
 
 		// act
 		res, err := solver.Solve(ctx, *question)
@@ -81,5 +92,7 @@ func TestLocalDnsSolver_SolveValidatingCache(t *testing.T) {
 		assert.Equal(t, "github.com.	0	IN	A	192.168.0.1", res.Answer[0].String())
 
 	}
+
+	mockCache.AssertExpectations(t)
 
 }
