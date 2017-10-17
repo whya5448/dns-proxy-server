cf203ff4ca9706e82330aa75bf2ec3e50dac6e7c
Elvis de Freitas <edigitalb@gmail.com>
2017-10-17 01:36:54 -0200

    #47 testing apis

---
+++ controller/cache_test.go
@@ -0,0 +1,40 @@
+package controller
+
+import (
+	"testing"
+	"net/http/httptest"
+	"github.com/go-resty/resty"
+	"github.com/stretchr/testify/assert"
+	"github.com/mageddo/dns-proxy-server/cache/store"
+)
+
+func TestGetCache(t *testing.T) {
+
+	s := httptest.NewServer(nil)
+	defer s.Close()
+
+	cache := store.GetInstance()
+	cache.Put("ke1", "value1")
+
+	r, err := resty.R().
+		Get(s.URL + CACHE_V1)
+	assert.Nil(t, err)
+	assert.Equal(t, `{"TEST_MODE":true,"ke1":"value1"}`, r.String())
+
+}
+
+func TestGetCacheSize(t *testing.T) {
+
+	s := httptest.NewServer(nil)
+	defer s.Close()
+
+	cache := store.GetInstance()
+	cache.Put("ke1", "value1")
+	cache.Put("ke2", "value2")
+
+	r, err := resty.R().
+		Get(s.URL + CACHE_SIZE_V1)
+	assert.Nil(t, err)
+	assert.Equal(t, `{"size":3}`, r.String())
+
+}
